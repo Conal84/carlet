0775f834c78fe7e9ed95a58ed9acd4400d9c6afd
Conal84 <conal.walsh1@gmail.com>
2020-10-26 17:51:08 +0000

    Added str methods to car and car image models

--- cars/models.py
+++ cars/models.py
@@ -12,7 +12,13 @@ class Car(models.Model):
     available = models.BooleanField()
     account = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return (self.make, self.model)
+
 
 class CarImage(models.Model):
     car_image = models.ImageField()
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.car_image.name)
