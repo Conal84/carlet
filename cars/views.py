cf43ac101ac1279c5d75c9191715e19ef4efcb3b
Conal84 <conal.walsh1@gmail.com>
2020-10-27 18:31:18 +0000

    Added search.html and tested functionality

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def search(request):
+    """ A view to return the search page """
+    template = 'cars/search.html'
+    context = {
+
+    }
+    return render(request, template, context)
