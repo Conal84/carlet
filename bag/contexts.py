fa2ef18d893a09017f2246b323848643f55ebcab
Conal84 <conal.walsh1@gmail.com>
2020-11-05 18:53:00 +0000

    Updated contexts.py with bag items and tested

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,5 @@
-from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from cars.models import Car
 
 
 def bag_contents(request):
@@ -6,6 +7,18 @@ def bag_contents(request):
     bag_items = []
     total = 0
     grand_total = 0
+    bag = request.session.get('bag', {})
+    print(bag.items())
+
+    for item_id, quantity in bag.items():
+        car = get_object_or_404(Car, pk=item_id)
+        total = car.cost_per_day
+        bag_items.append({
+            'item_id': item_id,
+            'car': car,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
