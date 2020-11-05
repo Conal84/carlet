b35a13095dd858ed58ed31e911b64e4b3332db40
Conal84 <conal.walsh1@gmail.com>
2020-11-05 17:17:21 +0000

    Completed view add to bag and tested functionality

--- bag/views.py
+++ bag/views.py
@@ -15,5 +15,10 @@ def add_to_bag(request, item_id):
 
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-    
+
+    bag[item_id] = 1
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+
     return redirect(redirect_url)
