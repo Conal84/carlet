869c1f7752e6314d394b60d13d9337d5abb494ca
Conal84 <conal.walsh1@gmail.com>
2020-11-14 17:24:12 +0000

    Added items to context dict in contexts.py and updated bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,14 +20,33 @@ def bag_contents(request):
     # grand_total = total
 
     # Total the contents of the bag
-    # for value in bag.values():
-    #     if type(value) == int:
-    #         grand_total += value
+    for value in bag.values():
+        if type(value) == int:
+            grand_total += value
 
-    grand_total = bag["car_cost"] + bag["insurance_cost"] + bag["support_cost"]
+    if bag:
+        id = bag["car_id"]
+        car = get_object_or_404(Car, pk=id)
+        make = car.make
+        model = car.model
+        car_cost_per_day = car.cost_per_day
+        num_days = car.num_days
+        car_total = car.car_total
+        insurance_per_day = car.insurance_per_day
+        insurance_total = car.insurance_total
+        support_per_day = car.support_per_day
+        support_total = car.support_total
 
     context = {
-        'bag': bag,
+        'make': make,
+        'model': model,
+        'car_cost_per_day': car_cost_per_day,
+        'num_days': num_days,
+        'car_total': car_total,
+        'insurance_per_day': insurance_per_day,
+        'insurance_total': insurance_total,
+        'support_per_day': support_per_day,
+        'support_total': support_total,
         'grand_total': grand_total,
     }
 
--- cars/models.py
+++ cars/models.py
@@ -31,17 +31,17 @@ class Car(models.Model):
     @property
     def car_total(self):
         tdelta = self.available_to - self.available_from
-        return self.cost_per_day * tdelta
+        return self.cost_per_day * tdelta.days
 
     @property
     def insurance_total(self):
         tdelta = self.available_to - self.available_from
-        return self.insurance_per_day * tdelta
+        return self.insurance_per_day * tdelta.days
 
     @property
     def support_total(self):
         tdelta = self.available_to - self.available_from
-        return self.support_per_day * tdelta
+        return self.support_per_day * tdelta.days
 
 
 # class Booking(models.Model):
