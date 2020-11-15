7261d58df21d3fca9148660c3dcb2ab501877477
Conal84 <conal.walsh1@gmail.com>
2020-11-15 12:10:48 +0000

    Added toast to display bag and tested functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,20 +4,8 @@ from cars.models import Car
 
 def bag_contents(request):
 
-    # bag_items = []
-    # total = 0
     grand_total = 0
     bag = request.session.get('bag', {})
-
-    # for item_id, quantity in bag.items():
-    #     car = get_object_or_404(Car, pk=item_id)
-    #     total += car.cost_per_day
-    #     bag_items.append({
-    #         'item_id': item_id,
-    #         'car': car,
-    #     })
-
-    # grand_total = total
 
     # Total the contents of the bag
     for value in bag.values():
