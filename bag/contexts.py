93c76ce18d07e69fbdb372b300104db094401d53
Conal84 <conal.walsh1@gmail.com>
2020-11-15 18:35:19 +0000

    Added nested if statements to context.py and tested functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,10 +20,6 @@ def bag_contents(request):
         car_cost_per_day = car.cost_per_day
         num_days = car.num_days
         car_total = car.car_total
-        insurance_per_day = car.insurance_per_day
-        insurance_total = car.insurance_total
-        support_per_day = car.support_per_day
-        support_total = car.support_total
 
         context = {
             'make': make,
@@ -31,12 +27,25 @@ def bag_contents(request):
             'car_cost_per_day': car_cost_per_day,
             'num_days': num_days,
             'car_total': car_total,
-            'insurance_per_day': insurance_per_day,
-            'insurance_total': insurance_total,
-            'support_per_day': support_per_day,
-            'support_total': support_total,
             'grand_total': grand_total,
         }
+
+        if "insurance_cost" in bag:
+            id = bag["car_id"]
+            car = get_object_or_404(Car, pk=id)
+            insurance_per_day = car.insurance_per_day
+            insurance_total = car.insurance_total
+            context["insurance_per_day"] = insurance_per_day
+            context["insurance_total"] = insurance_total
+
+            if "support_cost" in bag:
+                id = bag["car_id"]
+                car = get_object_or_404(Car, pk=id)
+                support_per_day = car.support_per_day
+                support_total = car.support_total
+                context["support_per_day"] = support_per_day
+                context["support_total"] = support_total
+
     else:
         context = {}
 
