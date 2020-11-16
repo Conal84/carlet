d49ec706a950155acc444bb9903374d1b62df7c8
Conal84 <conal.walsh1@gmail.com>
2020-11-16 17:36:38 +0000

    Trying to fix broken urls for car support

--- cars/urls.py
+++ cars/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('car-insurance/', views.car_insurance, name='car insurance'),
     path('car-insurance/<car_id>', views.car_insurance, name='car insurance'),
     path('car-support/', views.car_support, name='car support'),
-    # path('car-support/<car_id>', views.car_support, name='car support'),
+    path('car-support/<car_id>', views.car_support, name='car support'),
 ]
