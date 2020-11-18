0f1f1de9de0056ad1b6ff845ab0c8f10fa57b680
Conal84 <conal.walsh1@gmail.com>
2020-11-18 22:50:53 +0000

    Changes to bag_contents to total correctly

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,20 +6,23 @@ def bag_contents(request):
 
     grand_total = 0
     bag = request.session.get('bag', {})
-
-    # Total the contents of the bag
-    # for value in bag.values():
-    #     if type(value) == int:
-    #         grand_total += value
-
     if "car_id" in bag:
         id = bag["car_id"]
         car = get_object_or_404(Car, pk=id)
+
+        # Total the contents of the bag
+        for value in bag.values():
+            if type(value) == int:
+                grand_total += value
+        grand_total = grand_total - id
+
+    if "car_id" in bag:
         make = car.make
         model = car.model
         car_cost_per_day = car.cost_per_day
         num_days = car.num_days
         car_total = car.car_total
+        # grand_total = car.car_total
 
         context = {
             'id': car.id,
@@ -28,7 +31,7 @@ def bag_contents(request):
             'car_cost_per_day': car_cost_per_day,
             'num_days': num_days,
             'car_total': car_total,
-            'grand_total': car_total,
+            'grand_total': grand_total,
         }
 
         if "insurance_cost" in bag:
@@ -36,20 +39,20 @@ def bag_contents(request):
             car = get_object_or_404(Car, pk=id)
             insurance_per_day = car.insurance_per_day
             insurance_total = car.insurance_total
-            grand_total = car.car_total + insurance_total
+            grand_total = grand_total + insurance_total
             context["insurance_per_day"] = insurance_per_day
             context["insurance_total"] = insurance_total
-            context["grand_total"] = grand_total
+            # context["grand_total"] = grand_total
 
             if "support_cost" in bag:
                 id = bag["car_id"]
                 car = get_object_or_404(Car, pk=id)
                 support_per_day = car.support_per_day
                 support_total = car.support_total
-                grand_total = car.car_total + insurance_total + support_total
+                grand_total = grand_total + support_total
                 context["support_per_day"] = support_per_day
                 context["support_total"] = support_total
-                context["grand_total"] = grand_total
+                # context["grand_total"] = grand_total
 
     else:
         context = {}
