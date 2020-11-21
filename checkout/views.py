5edb07664cb526aeccdbbe332d1821aa4c6174b6
Conal84 <conal.walsh1@gmail.com>
2020-11-21 15:11:47 +0000

    Added checkout html, crispy forms built in in settings.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
 
 
 def checkout(request):
-    """ A view to render the shopping bag """
+    """ A view to render the checkout form """
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag in empty at the moment")
+        return redirect(reverse('all cars'))
+
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
-    return render(request, template)
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
