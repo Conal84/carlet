f8f58a12f69ee8378a785038fd0bca5079dc5139
Conal84 <conal.walsh1@gmail.com>
2020-12-08 11:12:01 +0000

    Tested CRUD functionality and fixed delete view error

--- cars/urls.py
+++ cars/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('add/', views.add_car, name='add_car'),
     path('display-cars/', views.display_cars, name='display_cars'),
     path('edit/<int:car_id>', views.edit_car, name='edit_car'),
-    path('delete/<int:car_id>', views.edit_car, name='delete_car'),
+    path('delete/<int:car_id>', views.delete_car, name='delete_car'),
 ]
