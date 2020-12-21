93a894a8c0feaf5c17b8ce25778f12e099eee763
Conal84 <conal.walsh1@gmail.com>
2020-12-21 18:38:21 +0000

    Moved google places to js file to prevent console errors when not needed

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,3 @@
-/**
- * A function to autocomplete city user input
- * @function activatePlacesSearch
- */
-function activatePlacesSearch() {
-  const options = {
-    types: ["(cities)"],
-    componentRestrictions: { country: "gb" },
-  };
-
-  let input = document.getElementById("search-location");
-  let autocomplete = new google.maps.places.Autocomplete(input, options);
-}
-
 $(document).ready(function () {
   /**
    * A click event to show the toast
