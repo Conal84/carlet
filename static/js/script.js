28558269f01abcafe15a4a94daafbc9c61671c78
Conal84 <conal.walsh1@gmail.com>
2020-12-21 18:26:18 +0000

    Moved checkout css and js to checkout app static

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,3 @@
-let checkinDate;
-let checkoutDate;
-let checkinDp;
-let checkoutDp;
-
 /**
  * A function to autocomplete city user input
  * @function activatePlacesSearch
@@ -19,66 +14,9 @@ function activatePlacesSearch() {
 
 $(document).ready(function () {
   /**
-   * A click event to how the toast
+   * A click event to show the toast
    */
   $("#bag-nav").click(function () {
     $(".toast").toast("show");
-  });
-
-  /**
-   * Create check in datepicker at html element
-   */
-  let checkinDiv = $('#search-from').datepicker({
-      weekStart: 1,
-      startDate: "today",
-      format: 'yyyy-mm-dd',
-      clearBtn: true,
-      autoclose: true,
-      todayHighlight: true,
-      orientation: "bottom",
-      beforeShowDay: function (date) {
-          if (checkoutDate !== undefined) {
-              if (date > checkoutDate) {
-                  return false;
-              }
-          }
-          return true;
-      }
-  });
-
-  checkinDp = checkinDiv.data('datepicker');
-  checkinDp.update(new Date());
-
-  checkinDiv.on('changeDate', (event) => {
-      checkinDate = event.date;
-      checkoutDp.update();
-      checkinDp.update();
-  });
-
-  /**
-   * Create check out datepicker at html element
-   */
-  let checkoutDiv = $('#search-to').datepicker({
-      weekStart: 1,
-      startDate: "today",
-      format: 'yyyy-mm-dd',
-      clearBtn: true,
-      autoclose: true,
-      todayHighlight: true,
-      orientation: "bottom",
-      beforeShowDay: function (date) {
-          if (checkinDate !== undefined) {
-              if (date < checkinDate) {
-                  return false;
-              }
-          }
-          return true;
-      }
-  });
-  checkoutDp = checkoutDiv.data('datepicker');
-  checkoutDiv.on('changeDate', (event) => {
-      checkoutDate = event.date;
-      checkinDp.update();
-      checkoutDp.update();
   });
 });
