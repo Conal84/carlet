b2e905443f18a39420a90672e8d7874218c36972
Conal84 <conal.walsh1@gmail.com>
2020-12-29 16:22:17 +0000

    Removed secret key and set debug

--- carlet/settings.py
+++ carlet/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x-f@j8=($^)^fp3$%_)hk$c6mw!-x6p#mv5@%4ct_qlr$+cl7t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['carlet-app.herokuapp.com', 'localhost']
 
