cb32eb3d8e748285def1a0e4857bad8be6df32f9
Conal84 <conal.walsh1@gmail.com>
2020-12-29 17:51:20 +0000

    Removed env import from settings.py

--- carlet/settings.py
+++ carlet/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, os
 import dj_database_url
-from os import path
-if path.exists("env.py"):
-  import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
