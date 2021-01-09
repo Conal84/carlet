0474150c381304d506557de3c4e82090027b19a6
Conal84 <conal.walsh1@gmail.com>
2021-01-09 11:34:59 +0000

    Added additional search query to cars_all

--- cars/views.py
+++ cars/views.py
@@ -30,19 +30,19 @@ def cars_all(request):
         # Calc the days between search days
         calc_days(request, search_from, search_to)
 
-        # cars = Car.objects.filter(
-        #     available=True
-        # ).filter(
-        #     location__icontains=location
-        # ).filter(
-        #     available_from__lte=search_from
-        # ).filter(
-        #     available_to__gte=search_to
-        # )
-
         cars = Car.objects.filter(
+            available=True
+        ).filter(
             city__icontains=location
+        ).filter(
+            available_from__lte=search_from
+        ).filter(
+            available_to__gte=search_to
         )
+
+        # cars = Car.objects.filter(
+        #     city__icontains=location
+        # )
         print(f"Cars by location are: {cars}")
         car_available_list = []
         for car in cars:
@@ -192,7 +192,11 @@ def display_cars(request):
         cars = Car.objects.all()
     else:
         user = request.user
-        cars = Car.objects.filter(user=user)
+        cars = Car.objects.filter(
+            user=user
+        ).filter(
+            available=True
+        )
 
     template = 'cars/display-cars.html'
     context = {
